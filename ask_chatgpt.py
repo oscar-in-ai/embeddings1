18a2e215967bd962906936f2edf248efc3f7a808
oscar-lopv <oscar.lopezv@hotmail.com>
2023-05-09 18:00:10 +1000

    Beautify

--- ask_chatgpt.py
+++ ask_chatgpt.py
@@ -1,9 +1,7 @@
 import os
 import openai
 import docx2txt
-
-api_key = os.getenv("openai_api_key")
-openai.api_key = api_key
+import pinecone
 
 def ask_gpt3(query):
     response = openai.Completion.create(
@@ -33,47 +31,39 @@ def get_openai_embedding(text):
         print(f"Error generating embedding: {str(e)}")
         return None
 
-
-query = "What is an ethical way to distribute the burdens of climate change?"
-
-
-response_no_context = ask_gpt3(query)
-embedding_vector = get_openai_embedding (query)
-
-
-
-import os
-import pinecone
-
-pinecone_api_key = os.getenv("pinecone_api_key")
-pinecone.init(api_key=pinecone_api_key, environment='us-east-1-aws')
-
-index_name = 'my-assignments2'
-namespace = 'assignments'
-
 def query_pinecone_index(embedding_vector):
     index = pinecone.Index(index_name=index_name)
     response = index.query(namespace=namespace,queries=[embedding_vector],top_k=2, include_metadata=True)
     results = response.results[0]['matches']
-    
-
-    # Assuming you have stored some metadata along with the embeddings,
-    # you can retrieve it using the `results.ids` and `results.distances` attributes.
-
-
     return results
+ 
 
+
+
+
+index_name = 'my-assignments2'
+namespace = 'assignments'
+openai.api_key = os.getenv("openai_api_key")
+pinecone_api_key = os.getenv("pinecone_api_key")
+pinecone.init(api_key=pinecone_api_key, environment='us-east-1-aws')
+
+query = "What is an ethical way to distribute the burdens of climate change?"
+query = "Who is Marychu Dayao"
+
+
+embedding_vector = get_openai_embedding (query)
 candidates = query_pinecone_index(embedding_vector)
 selected_file = candidates[0]['metadata']['file_name']
-print(selected_file)
+print(f'File candidates:\n{candidates}')
 
 my_context = text = docx2txt.process(selected_file)
 
+response_no_context = ask_gpt3(query)
 response_with_context = ask_gpt3(f"Answer this question: {query} using the following document as context only if appropriate: {my_context}")
 
-print(response_no_context)
+print(f"Response with NO context:\n{response_no_context}")
 print('-------------------')
-print(response_with_context)
+print(f"Response with context:\n{response_with_context}")
 
 
 
